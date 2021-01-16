c1ed3cc37146195e5b9f300bcf298b5246f3b058
Scott McAllister <smcallister@pagerduty.com>
2021-01-15 18:29:45 -0800

    add layer to sched

--- main.tf
+++ main.tf
@@ -53,6 +53,9 @@ resource "pagerduty_escalation_policy" "ep" {
   }
 }
 
+data "pagerduty_user" "susan" {
+  email = "susan.devel@gmail.com"
+}
 
 # /* SCHEDULE */
 resource "pagerduty_schedule" "foo" {
@@ -60,16 +63,30 @@ resource "pagerduty_schedule" "foo" {
   time_zone = "America/Los_Angeles"
 
   layer {
-    name                         = "Night Shift"
-    start                        = "2020-09-20T20:00:00-07:00"
-    rotation_virtual_start       = "2020-09-20T20:00:00-07:00"
+    name                         = "Day Shift"
+    start                        = "2020-12-07T20:00:00-08:00"
+    rotation_virtual_start       = "2020-12-07T08:00:00-08:00"
     rotation_turn_length_seconds = 86400
-    users                        = [pagerduty_user.lisa.id, pagerduty_user.bart.id]
+    users                        = [data.pagerduty_user.susan.id]
 
     restriction {
       type              = "daily_restriction"
       start_time_of_day = "08:00:00"
       duration_seconds  = 32400
+    }
+  }
+  
+  layer {
+    name                         = "Night Shift"
+    start                        = "2020-12-07T20:00:00-08:00"
+    rotation_virtual_start       = "2020-12-07T17:00:00-08:00"
+    rotation_turn_length_seconds = 86400
+    users                        = [pagerduty_user.lisa.id, pagerduty_user.bart.id]
+
+    restriction {
+      type              = "daily_restriction"
+      start_time_of_day = "17:00:00"
+      duration_seconds  = 54000
     }
   }
 }
