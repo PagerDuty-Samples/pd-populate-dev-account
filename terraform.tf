56b9de6e7724a775f189fd27925ed8641ed41527
Todd Whitney <twhitney@pagerduty.com>
2023-03-14 11:24:31 -0400

    git ignore and other files

--- terraform.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    # see https://registry.terraform.io/providers/PagerDuty/pagerduty/2.7.0
-    pagerduty = {
-      source  = "PagerDuty/pagerduty"
-      version = "2.7.0"
-    }
-  }
-}
